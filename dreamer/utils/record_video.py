b757840a444b553eb736429fbdd984dbc1edddb5
makolon <s.makolon@icloud.com>
2022-10-08 21:47:37 +0900

    add reacord_video.py for debug

---
+++ dreamer/utils/record_video.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+img_size = (256, 256)
+
+def create_video(images):
+    fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+    writer = cv2.VideoWriter('sample.mp4', fourcc, 20, (256, 256))
+    for i in range(len(images)):
+        img = images[i]
+        img_np = np.array(img).astype('uint8')
+        img_cv = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        writer.write(img_cv)
+    writer.release()
