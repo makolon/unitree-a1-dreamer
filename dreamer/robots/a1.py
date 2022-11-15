8b7876cc7346748d5513f2d9f69aaa4c8ca4c783
makolon <s.makolon@icloud.com>
2022-11-15 17:28:39 +0900

    fix env bug

--- dreamer/robots/a1.py
+++ dreamer/robots/a1.py
@@ -85,6 +85,72 @@ _DEFAULT_HIP_POSITIONS = (
 # KNEE_P_GAIN = 100.0
 # KNEE_D_GAIN = 2.0
 
+MOTOR_NAMES = [
+    "FR_hip_joint",
+    "FR_upper_joint",
+    "FR_lower_joint",
+    "FL_hip_joint",
+    "FL_upper_joint",
+    "FL_lower_joint",
+    "RR_hip_joint",
+    "RR_upper_joint",
+    "RR_lower_joint",
+    "RL_hip_joint",
+    "RL_upper_joint",
+    "RL_lower_joint",
+]
+
+MOTOR_MINS = np.array([
+    -0.802851455917,
+    -1.0471975512,
+    -2.69653369433,
+] * 4)
+
+MOTOR_MAXS = np.array([
+    0.802851455917,
+    4.18879020479,
+    -0.916297857297,
+] * 4)
+
+MOTOR_OFFSETS = np.array([
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+    0.0, 0.0, 0.0,
+])
+
+MOTOR_USED = np.array([
+    [0.01, 0.99],
+    [0.01, 0.90],
+    [0.01, 0.60],
+] * 4)
+
+STANDING_POSE = np.array([0, -0.2, 1.0] * 4)
+
+def unnormalize_action(action, clip=True):
+  if clip:
+    action = np.clip(action, -1, 1)
+  action = action / 2 + 0.5
+  lo = MOTOR_MINS * (1 - MOTOR_USED[:, 0]) + MOTOR_MAXS * MOTOR_USED[:, 0]
+  hi = MOTOR_MINS * (1 - MOTOR_USED[:, 1]) + MOTOR_MAXS * MOTOR_USED[:, 1]
+  action = action * (hi - lo) + lo
+  action += MOTOR_OFFSETS
+  return action
+
+def normalize_action(action, clip=True):
+  action -= MOTOR_OFFSETS
+  lo = MOTOR_MINS * (1 - MOTOR_USED[:, 0]) + MOTOR_MAXS * MOTOR_USED[:, 0]
+  hi = MOTOR_MINS * (1 - MOTOR_USED[:, 1]) + MOTOR_MAXS * MOTOR_USED[:, 1]
+  action = (action - lo) / (hi - lo)
+  action = (action - 0.5) * 2
+  if clip:
+    action = np.clip(action, -1, 1)
+  return action
+
+COM_OFFSET = -np.array([0.012731, 0.002186, 0.000515])
+HIP_OFFSETS = np.array([[0.183, -0.047, 0.], [0.183, 0.047, 0.],
+                        [-0.183, -0.047, 0.], [-0.183, 0.047, 0.]
+                        ]) + COM_OFFSET
 
 ABDUCTION_P_GAIN = 80.0
 ABDUCTION_D_GAIN = 0.4
@@ -107,6 +173,89 @@ URDF_FILENAME = "a1/a1.urdf"
 _BODY_B_FIELD_NUMBER = 2
 _LINK_A_FIELD_NUMBER = 3
 
+# Found that these numba.jit decorators slow down the timestep from 1ms without
+# to 5ms with decorators.
+# @numba.jit(nopython=True, cache=True)
+def foot_position_in_hip_frame_to_joint_angle(foot_position, l_hip_sign=1):
+  l_up = 0.2
+  l_low = 0.2
+  l_hip = 0.08505 * l_hip_sign
+  x, y, z = foot_position[0], foot_position[1], foot_position[2]
+  theta_knee = -np.arccos(
+      (x**2 + y**2 + z**2 - l_hip**2 - l_low**2 - l_up**2) /
+      (2 * l_low * l_up))
+  l = np.sqrt(l_up**2 + l_low**2 + 2 * l_up * l_low * np.cos(theta_knee))
+  theta_hip = np.arcsin(-x / l) - theta_knee / 2
+  c1 = l_hip * y - l * np.cos(theta_hip + theta_knee / 2) * z
+  s1 = l * np.cos(theta_hip + theta_knee / 2) * y + l_hip * z
+  theta_ab = np.arctan2(s1, c1)
+  return np.array([theta_ab, theta_hip, theta_knee])
+
+
+# @numba.jit(nopython=True, cache=True)
+def foot_position_in_hip_frame(angles, l_hip_sign=1):
+  theta_ab, theta_hip, theta_knee = angles[0], angles[1], angles[2]
+  l_up = 0.2
+  l_low = 0.2
+  l_hip = 0.08505 * l_hip_sign
+  leg_distance = np.sqrt(l_up**2 + l_low**2 +
+                         2 * l_up * l_low * np.cos(theta_knee))
+  eff_swing = theta_hip + theta_knee / 2
+
+  off_x_hip = -leg_distance * np.sin(eff_swing)
+  off_z_hip = -leg_distance * np.cos(eff_swing)
+  off_y_hip = l_hip
+
+  off_x = off_x_hip
+  off_y = np.cos(theta_ab) * off_y_hip - np.sin(theta_ab) * off_z_hip
+  off_z = np.sin(theta_ab) * off_y_hip + np.cos(theta_ab) * off_z_hip
+  return np.array([off_x, off_y, off_z])
+
+
+# @numba.jit(nopython=True, cache=True)
+def analytical_leg_jacobian(leg_angles, leg_id):
+  """
+  Computes the analytical Jacobian.
+  Args:
+  ` leg_angles: a list of 3 numbers for current abduction, hip and knee angle.
+    l_hip_sign: whether it's a left (1) or right(-1) leg.
+  """
+  l_up = 0.2
+  l_low = 0.2
+  l_hip = 0.08505 * (-1)**(leg_id + 1)
+
+  t1, t2, t3 = leg_angles[0], leg_angles[1], leg_angles[2]
+  l_eff = np.sqrt(l_up**2 + l_low**2 + 2 * l_up * l_low * np.cos(t3))
+  t_eff = t2 + t3 / 2
+  J = np.zeros((3, 3))
+  J[0, 0] = 0
+  J[0, 1] = -l_eff * np.cos(t_eff)
+  J[0, 2] = l_low * l_up * np.sin(t3) * np.sin(t_eff) / l_eff - l_eff * np.cos(
+      t_eff) / 2
+  J[1, 0] = -l_hip * np.sin(t1) + l_eff * np.cos(t1) * np.cos(t_eff)
+  J[1, 1] = -l_eff * np.sin(t1) * np.sin(t_eff)
+  J[1, 2] = -l_low * l_up * np.sin(t1) * np.sin(t3) * np.cos(
+      t_eff) / l_eff - l_eff * np.sin(t1) * np.sin(t_eff) / 2
+  J[2, 0] = l_hip * np.cos(t1) + l_eff * np.sin(t1) * np.cos(t_eff)
+  J[2, 1] = l_eff * np.sin(t_eff) * np.cos(t1)
+  J[2, 2] = l_low * l_up * np.sin(t3) * np.cos(t1) * np.cos(
+      t_eff) / l_eff + l_eff * np.sin(t_eff) * np.cos(t1) / 2
+  return J
+
+
+# For JIT compilation
+foot_position_in_hip_frame_to_joint_angle(np.random.uniform(size=3), 1)
+foot_position_in_hip_frame_to_joint_angle(np.random.uniform(size=3), -1)
+
+
+# @numba.jit(nopython=True, cache=True, parallel=True)
+def foot_positions_in_base_frame(foot_angles):
+  foot_angles = foot_angles.reshape((4, 3))
+  foot_positions = np.zeros((4, 3))
+  for i in range(4):
+    foot_positions[i] = foot_position_in_hip_frame(foot_angles[i],
+                                                   l_hip_sign=(-1)**(i + 1))
+  return foot_positions + HIP_OFFSETS
 
 class A1(minitaur.Minitaur):
   """A simulation for the Laikago robot."""
